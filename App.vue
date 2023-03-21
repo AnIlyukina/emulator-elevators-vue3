77b72ab89793cbc46fcacb44846c9dcdc2995844
Ilyukina <anyailyukina@yandex.ru>
2023-03-21 21:22:43 +0800

    инициализация проекта, первоначальная сборка модулей webpack

---
+++ App.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    Эмулятор лифта
+  </div>
+</template>
+
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require("webpack");
+
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src", "main.js"),
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname),
+    },
+    compress: true,
+    port: 8081,
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/dist/",
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      { test: /\.vue$/, loader: "vue-loader" },
+      {
+        test: /\.(scss|css)$/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      }
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      // Drop Options API from bundle
+      __VUE_OPTIONS_API__: false,
+      __VUE_PROD_DEVTOOLS__: true,
+    }),
+  ],
+};
