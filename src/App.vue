1ba2a295ac178db4ec53d8820f6c0de21010cb64
AnnaIlyukina <anyailyukina@yandex.ru>
2023-05-05 16:14:35 +0800

    добавила компонент с конфигурационными параметрами

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script setup>
 import Shafts from "./components/shafts.vue";
+import ConfigParams from './components/config-params.vue'
+
+import { countElevators, countFloor, floorHeight } from "../elevator-config";
+import { reactive } from 'vue';
+
+const defaultConfiguration = {
+  countFloor,
+  countElevators,
+  floorHeight
+}
+
+const configuration = reactive({
+  ...defaultConfiguration
+})
+
 
 </script>
 
 <template>
-  <shafts/>
+  <config-params
+    :configuration="configuration"
+  />
+  <shafts
+    :configuration="configuration"
+  />
 </template>
 
 
